e99f8b31cbf5b4f6b5c3777c9de1cef222b16c20
hiddencilantro <stevekim1109@gmail.com>
2020-10-16 04:35:23 +0900

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,6 +16,6 @@ class String
 
   def count_sentences
     divided_sentences = self.split(/[.?!]/).reject{|string| string.length == 0}
-    divided_sentences.length
+    divided_sentences.count
   end
 end
